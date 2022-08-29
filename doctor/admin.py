2246827bf87f5fe689eb9082724071ba6d522529
Aman Ray <96371053+amanray33541@users.noreply.github.com>
2022-08-29 22:29:20 +0600

    final project

--- doctor/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(Doctor)
-admin.site.register(DegreeOfDoctor)
-admin.site.register(Chamber)
-admin.site.register(Appointment)
